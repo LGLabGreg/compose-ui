5269babf497be666513089a1ce7825bed25dd575
LGLabGreg <greg@lglab.co.uk>
2026-02-07 17:48:51 +0000

    nit(breadcrumb): chevronright

--- apps/docs/app/(docs)/components/breadcrumb/examples/with-custom-separator.tsx
+++ apps/docs/app/(docs)/components/breadcrumb/examples/with-custom-separator.tsx
@@ -7,24 +7,7 @@ import {
   BreadcrumbRoot,
   BreadcrumbSeparator,
 } from '@lglab/compose-ui/breadcrumb'
-
-function ChevronRight() {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='16'
-      height='16'
-      viewBox='0 0 24 24'
-      fill='none'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-    >
-      <path d='m9 18 6-6-6-6' />
-    </svg>
-  )
-}
+import { ChevronRight } from 'lucide-react'
 
 export default function WithCustomSeparatorExample() {
   return (
